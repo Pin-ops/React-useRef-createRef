9800ee3c9d7c3bfadae7dd25774ec48a3de0bd3a
Pin-ops <pinozmen89@gmail.com>
2020-04-22 21:08:23 +0200

    Updates

---
+++ precache-manifest.81f73cf540d3c13e67cbce6994060285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766ed706ac6287e17aad26626dfeedfc",
+    "url": "/React-useRef-createRef/index.html"
+  },
+  {
+    "revision": "8b7e42a96c5d308a9cbb",
+    "url": "/React-useRef-createRef/static/css/main.da5d7109.chunk.css"
+  },
+  {
+    "revision": "c4fc4bb1df4f8b3a60e1",
+    "url": "/React-useRef-createRef/static/js/2.ca900440.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-useRef-createRef/static/js/2.ca900440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7e42a96c5d308a9cbb",
+    "url": "/React-useRef-createRef/static/js/main.83d2dd67.chunk.js"
+  },
+  {
+    "revision": "dbfce782b7c3397d2bae",
+    "url": "/React-useRef-createRef/static/js/runtime-main.a253a9c0.js"
+  }
+]);
